bc7c5633a330a008e8b916b31d8b668ccf45bbd9
Gordon Hatcher <gordolego@gmail.com>
2025-06-23 03:11:58 +0000

    Make the borders for those image containers invisible. Also make the pic

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -328,7 +328,7 @@ export default function ProjectDetailPage({ params }: ProjectDetailPageProps) {
                               
                                 <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 justify-items-center">
                                   {step.images.map((image, imgIndex) => (
-                                    <figure key={imgIndex} className="w-full flex flex-col">
+                                    <figure key={imgIndex} className="w-3/4 flex flex-col">
                                       <div className="relative w-full aspect-[4/3] rounded-lg overflow-hidden shadow-md bg-muted/10">
                                         <Image
                                           src={image.src}
@@ -407,7 +407,7 @@ export default function ProjectDetailPage({ params }: ProjectDetailPageProps) {
                                 <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
                                   {step.images.slice(0, 2).map((image, imgIndex) => (
                                     <figure key={imgIndex} className="w-full flex flex-col">
-                                      <div className="relative w-[90%] mx-auto aspect-[4/3] rounded-lg overflow-hidden shadow-md bg-muted/10">
+                                      <div className="relative w-[90%] mx-auto aspect-[4/3] rounded-lg overflow-hidden">
                                         <Image
                                           src={image.src}
                                           alt={image.alt}
@@ -422,7 +422,7 @@ export default function ProjectDetailPage({ params }: ProjectDetailPageProps) {
                                   {step.images.length > 2 && (
                                     <div className="sm:col-span-2 flex justify-center">
                                        <figure className="w-full max-w-[calc(50%-0.5rem)] flex flex-col">
-                                        <div className="relative w-full aspect-[10/1] rounded-lg overflow-hidden shadow-md bg-muted/10">
+                                        <div className="relative w-full aspect-[10/1] rounded-lg overflow-hidden">
                                             <Image
                                             src={step.images[2].src}
                                             alt={step.images[2].alt}
