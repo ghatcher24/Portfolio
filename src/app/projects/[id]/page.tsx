aae7192601b4c461a0eb0febda2b0d9bcc4eb533
Gordon Hatcher <gordolego@gmail.com>
2025-07-01 03:21:00 +0000

    src/app/projects/[id]/page.tsx Type error: Type '{ params: { id: string;

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -494,7 +494,7 @@ export default function ProjectDetailPage({ params }: { params: { id: string } }
                                 {step.images.map((image, imgIndex) => {
                                   const aspectClass = image.src.includes('Schedule') ? 'aspect-[2/1]' : (image.src.includes('Risk') ? 'aspect-[3/1]' : 'aspect-video');
                                   return (
-                                    <div key={imgIndex} className={`relative w-full md:w-2/5 ${aspectClass} rounded-lg overflow-hidden shadow-md`}>
+                                    <div key={imgIndex} className={`relative w-full md:w-3/5 ${aspectClass} rounded-lg overflow-hidden shadow-md`}>
                                         <Image
                                           src={image.src}
                                           alt={image.alt}
@@ -579,7 +579,7 @@ export default function ProjectDetailPage({ params }: { params: { id: string } }
                                 {step.images.map((image, imgIndex) => {
                                   const aspectClass = 'aspect-[822/658]';
                                   return (
-                                    <div key={imgIndex} className={`relative w-full ${step.title.includes('Untreated Model') ? 'md:w-1/2' : 'md:w-2/5'} ${aspectClass} rounded-lg overflow-hidden shadow-md`}>
+                                    <div key={imgIndex} className={`relative w-full ${step.title.includes('Untreated Model') ? 'md:w-1/2' : 'md:w-3/5'} ${aspectClass} rounded-lg overflow-hidden shadow-md`}>
                                       <Image
                                         src={image.src}
                                         alt={image.alt}
@@ -628,7 +628,7 @@ export default function ProjectDetailPage({ params }: { params: { id: string } }
                                 {step.images.map((image, imgIndex) => {
                                   const aspectClass = image.src.includes('Concepting') ? 'aspect-[1918/1346]' : 'aspect-[4/3]';
                                   return (
-                                    <div key={imgIndex} className={`relative w-full md:w-4/5 ${aspectClass} rounded-lg overflow-hidden shadow-md`}>
+                                    <div key={imgIndex} className={`relative w-full md:w-3/5 ${aspectClass} rounded-lg overflow-hidden shadow-md`}>
                                         <Image
                                           src={image.src}
                                           alt={image.alt}
@@ -652,7 +652,7 @@ export default function ProjectDetailPage({ params }: { params: { id: string } }
                               <div className="mt-4 flex justify-center">
                                 {step.images.map((image, imgIndex) => {
                                   return (
-                                    <div key={imgIndex} className={`relative w-full md:w-4/5 aspect-[4/3] rounded-lg overflow-hidden shadow-md`}>
+                                    <div key={imgIndex} className={`relative w-full md:w-3/5 aspect-[4/3] rounded-lg overflow-hidden shadow-md`}>
                                         <Image
                                           src={image.src}
                                           alt={image.alt}
@@ -676,7 +676,7 @@ export default function ProjectDetailPage({ params }: { params: { id: string } }
                               <div className="mt-4 flex justify-center">
                                 {step.images.map((image, imgIndex) => {
                                   return (
-                                    <div key={imgIndex} className={`relative w-full md:w-4/5 aspect-video rounded-lg overflow-hidden shadow-md`}>
+                                    <div key={imgIndex} className={`relative w-full md:w-3/5 aspect-video rounded-lg overflow-hidden shadow-md`}>
                                         <Image
                                           src={image.src}
                                           alt={image.alt}
@@ -700,7 +700,7 @@ export default function ProjectDetailPage({ params }: { params: { id: string } }
                               <div className="mt-4 flex justify-center">
                                 {step.images.map((image, imgIndex) => {
                                   return (
-                                    <div key={imgIndex} className={`relative w-full md:w-4/5 aspect-video rounded-lg overflow-hidden shadow-md`}>
+                                    <div key={imgIndex} className={`relative w-full md:w-3/5 aspect-video rounded-lg overflow-hidden shadow-md`}>
                                         <Image
                                           src={image.src}
                                           alt={image.alt}
@@ -760,7 +760,7 @@ export default function ProjectDetailPage({ params }: { params: { id: string } }
               <section className="mb-8 prose prose-lg max-w-none text-foreground">
                 <h2 className="text-2xl font-semibold text-accent mb-3 flex items-center justify-center"><Images size={24} className="mr-3 text-primary" />Manufacturing Drawings</h2>
                 <div className="mt-6 flex justify-center">
-                    <Carousel className="w-full max-w-2xl">
+                    <Carousel className="w-full max-w-lg">
                       <CarouselContent>
                         {Array.from({ length: 13 }).map((_, index) => (
                           <CarouselItem key={index}>
